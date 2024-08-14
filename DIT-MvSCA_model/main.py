238d5217932cf9059d5082d05c543b76954a539a
BzzMS <49432146+BzzMS@users.noreply.github.com>
2024-08-14 13:27:23 +0800

    bug fix
    bug fix

--- DIT-MvSCA_model/main.py
+++ DIT-MvSCA_model/main.py
@@ -9,24 +9,28 @@ from model import GNN
 from torch_geometric.loader import ShaDowKHopSampler
 from config import Config
 from torch_geometric.data import Data
+from sklearn.metrics import auc as auc3
+from sklearn.metrics import roc_auc_score, precision_recall_curve, accuracy_score
 from tqdm import tqdm
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+cuda_num = 0
 
-
-cuda_num=0
 if __name__ == "__main__":
-    config_file = "./config/dti_v3.ini"
+    # config_file = "./config/dti_v3.ini"
+    config_file = "./config/dti_v5.ini"
     config = Config(config_file)
     fold_ROC = []
     fold_AUPR = []
     criterion = nn.CrossEntropyLoss()
-    for fold in range(5):
-        config.structgraph_path = "../data_v3/alledg.txt"
-        config.train_path = "../data_v3/train00{}.txt".format(fold + 1)
-        config.test_path = "../data_v3/test00{}.txt".format(fold + 1)
-        # config.structgraph_path = "../data_v5/alledg.txt"
-        # config.train_path = "../data_v5/train00{}.txt".format(fold + 1)
-        # config.test_path = "../data_v5/test00{}.txt".format(fold + 1)
+    my_depth = config.depth
+    my_num_neighbors = config.num_neighbors
+    batch_size = config.batch_size
+    for fold in range(0, 5):
+        # config.structgraph_path = "../data_v3/alledg.txt"
+        # config.train_path = "../data_v3/train00{}.txt".format(fold + 1)
+        # config.test_path = "../data_v3/test00{}.txt".format(fold + 1)
+        config.structgraph_path = "../data_v5/alledg.txt"
+        config.train_path = "../data_v5/train00{}.txt".format(fold + 1)
+        config.test_path = "../data_v5/test00{}.txt".format(fold + 1)
         random.seed(config.seed)
         np.random.seed(config.seed)
         torch.manual_seed(config.seed)
@@ -34,70 +38,64 @@ if __name__ == "__main__":
         torch.cuda.manual_seed_all(config.seed)
         torch.backends.cudnn.benchmark = False  # Close optimization
         torch.backends.cudnn.deterministic = True  # Close optimization
-
         sadj= load_graph(config)
         features, labels, idx_train, idx_test = load_data(config)
-
         asadj = get_adj(sadj)
         model = GNN(in_channels=config.fdim,
                     hidden_channels1=config.nhid1,
                     hidden_channels2=config.nhid2,
                     nclass=config.class_num
                     ).cuda(cuda_num)
-
         features = features
         sadj = sadj
+
         labels = labels
         idx_train = idx_train
         idx_test = idx_test
         boo_train = index_to_boo(features.shape[0], idx_train)
         boo_test = index_to_boo(features.shape[0], idx_test)
         s_values = sadj._values()
+
         labels_shape=labels.shape[0]
         tmp_ten=torch.arange(0,labels_shape,1).reshape(labels_shape,1)
         labels=torch.cat((labels.reshape(labels_shape,1),tmp_ten),1)
+
         s_data = Data(x=features, edge_index=asadj, edge_attr=s_values, y=labels, train_mask=boo_train,
                       test_mask=boo_test).cuda(cuda_num)
 
-        my_depth = 3
-        my_num_neighbors = 12
-        batch_size=18
+
         s_train_loader = ShaDowKHopSampler(s_data, depth=my_depth, num_neighbors=my_num_neighbors,
                                            node_idx=s_data.train_mask, shuffle=True, batch_size=batch_size)
         s_test_loader = ShaDowKHopSampler(s_data, depth=my_depth, num_neighbors=my_num_neighbors,
-                                          node_idx=s_data.test_mask, shuffle=True)
+                                          node_idx=s_data.test_mask, shuffle=True, batch_size=batch_size)
 
 
 
 
-        optimizer = optim.Adam(model.parameters(), lr=0.0003, weight_decay=config.weight_decay)
-        scheduler = ReduceLROnPlateau(optimizer, 'min',verbose=True,factor=0.8,patience=8,min_lr=0.00003)
+        optimizer = optim.Adam(model.parameters(), lr=config.lr, weight_decay=config.weight_decay)
+
         roc = []
 
         pr = []
         acc = []
+        all_lable = []
+        all_pred = []
 
-        def train(model, epochs):
+        def train(model):
             model.train()
             num=0
             total_loss=0
-
             for s_data in tqdm(s_train_loader):
-
                 s_data=s_data.cuda(cuda_num)
-
                 optimizer.zero_grad()
                 output = model(s_data.x, s_data.edge_index, s_data.batch, s_data.root_n_id).cuda(cuda_num)
-
                 tmp_lable=s_data.y[:,0]
-
                 loss = criterion(output, tmp_lable)
                 loss.backward()
                 optimizer.step()
                 num=num+1
                 total_loss += loss
                 real_loss= total_loss / num
-            scheduler.step(real_loss)
             return real_loss
         def main_test(model, epoch, loss):
             model.eval()
@@ -106,20 +104,21 @@ if __name__ == "__main__":
             for s_data in tqdm(s_test_loader):
                 s_data = s_data.cuda(cuda_num)
                 output = model(s_data.x, s_data.edge_index, s_data.batch, s_data.root_n_id).cuda(cuda_num)
-                outputs.append(output.cpu().detach().numpy())
-                label.append(int(s_data.y[0][0].cpu()))
-            label = np.array(label)
-            label = torch.tensor(label)
-            outputs = np.array(outputs)
-            outputs = torch.tensor(outputs).squeeze()
-            c, d = RocAndAupr(outputs, label)
-            acc_test = accuracy(outputs, label)
-            roc.append(c)
-            pr.append(d)
+                outputs.append(output.cpu().detach())
+                label.append(s_data.y[:,0].cpu())
+            label_test = torch.cat(label)
+            outputs_test = torch.cat(outputs)[:, 1]
+            auc = roc_auc_score(label_test, outputs_test)
+            precision, recall, thresholds = precision_recall_curve(label_test, outputs_test)
+            aupr = auc3(recall, precision)
+            y_pred = (outputs_test>= 0.5).to(torch.int32)
+            acc_test = accuracy_score(label_test, y_pred)
+            roc.append(auc)
+            pr.append(aupr)
             acc.append(acc_test)
 
             print("this is the", epoch + 1,
-                  "epochs, ROC is {:.4f},and AUPR is {:.4f} test set accuray is {:.4f},loss is {:.4f} ".format(c, d, acc_test,
+                  "epochs, ROC is {:.4f},and AUPR is {:.4f} test set accuray is {:.4f},loss is {:.4f} ".format(auc, aupr, acc_test,
                                                                                                                loss))
 
 
@@ -130,7 +129,7 @@ if __name__ == "__main__":
 
         for epoch in range(config.epochs):
 
-            loss=train(model, epoch)
+            loss=train(model)
             main_test(model, epoch, loss)
             if acc_max < acc[epoch]:
                 acc_max = acc[epoch]
@@ -148,3 +147,4 @@ if __name__ == "__main__":
                         acc_max))
     print("average AUROC is {:.4} , average AUPR is {:.4}".format(sum(fold_ROC) / len(fold_ROC),
                                                                   sum(fold_AUPR) / len(fold_AUPR)))
+
